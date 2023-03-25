bc83b0c9a359aecfba9718503a90f91ca67229f3
sarath-321 <128903187+sarath-321@users.noreply.github.com>
2023-03-25 17:12:18 +0530

    Add files via upload
    Week1_Day1 Assignment completed..

---
+++ Assignments/src/main/java/week1/day1/Car.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Car {
+
+	public void applyBrake() {
+		System.out.println("Applied Brake");
+	}
+	
+	public void soundHorn() {
+		System.out.println("Keeeng keeeng!");
+	}
+}
---
+++ Assignments/src/main/java/week1/day1/Chrome.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class Chrome {
+
+	public void PrintName() {
+		System.out.println("This is google chrome");
+	}
+
+	public void getName() {
+		System.out.println("Owned by Google");
+	}
+}
